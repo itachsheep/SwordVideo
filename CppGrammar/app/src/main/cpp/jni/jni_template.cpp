61b38b5d094f2e41789ac2997508eb03c5d717f2
itachsheep <itachsheep@163.com>
2022-03-04 14:23:55 +0800

    cpp 语法工程
    -----------------------------
    template 用法

--- CppGrammar/app/src/main/cpp/jni/jni_template.cpp
+++ CppGrammar/app/src/main/cpp/jni/jni_template.cpp
@@ -9,17 +9,42 @@ using namespace std;
 
 
 template<typename T>
+int compare(T& left, T& right) {
+    if(left < right) {
+        return  -1;
+    } else if( left > right) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+template<typename T>
 void func(T& t) {
     cout << "In generic version template t = " << t  << endl;
-//    LOGD("func In generic version template t = %d ",t);
 }
+
+template<typename T>
+void func(T *t) {
+    cout << "In pointer version template *t = " << *t << endl;
+}
+
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_tao_myndktest_NativeHelper_testTemplate(JNIEnv *env, jobject thiz) {
     //simpleLog();
-//    LOGD("------>  111 ");
     cout.rdbuf( new AndroidBuf());
     int a = 100;
     func(a);
+    int x = 100,y = 9;
+    int result = compare(x,y);
+    cout << "result = " << result << endl;
+
+    char *s = "hello ni hao !!!";
+    string ss = " ni hao a !!!";
+    func(&s);
+    func(s);
+    func(&ss);
+
 }
